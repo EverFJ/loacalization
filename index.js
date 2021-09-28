1bd85ba934853ffa09a0b9565b27d4a957ff6ad4
Francois Janson <jansonfrancois@gmail.com>
2021-09-28 14:30:16 +0200

    dynamic greeting

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
-const port = 8000;
-
 const app = express();
+const port = 8000;
+const translations = require("./translations.json");
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+app.get("/:lang?", (req, res) => {
+    const lang = req.params.lang ? req.params.lang : "fr";
+    res.render("home", {
+        greeting: translations[lang]
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
